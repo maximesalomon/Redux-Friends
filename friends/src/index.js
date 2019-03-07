1c00f2e8561aad75d9e048352ecae7cf051ec591
Maxime Salomon <maxime@croissant.io>
2019-03-07 16:02:51 +0100

    added reducers

--- friends/src/index.js
+++ friends/src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import { createStore, applyMiddleware} from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import rootreducer from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootreducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>,
+document.getElementById('root'));
---
+++ friends/src/reducers/index.js
@@ -0,0 +1,40 @@
+import { FETCHING, UPDATING, DELETING, SUCCESS, FAILURE, ADDING, UPDATE_FORM } from '../actions';
+
+const initialState = {
+  friends: [],
+  friendToUpdate: null,
+  fetching: false,
+  updating: false,
+  deleting: false,
+  adding: false,
+  error: null
+}
+
+export default function reduce(state = initialState, action) {
+  switch (action.type) {
+    case FETCHING:
+      return {...state, fetching: true}
+    case UPDATING:
+      return {...state, updating: true}
+    case DELETING:
+      return {...state, deleting: true}
+    case ADDING:
+      return {...state, adding: true}
+    case SUCCESS:
+      return {
+        friends: action.payload,
+        fetching: false,
+        updating: false,
+        deleting: false,
+        adding: false,
+        failed: false,
+        friendToUpdate: null
+      }
+    case FAILURE:
+      return {...state, error: action.payload}
+    case UPDATE_FORM:
+      return {...state, friendToUpdate: action.payload}
+    default:
+      return state;
+  }
+}
